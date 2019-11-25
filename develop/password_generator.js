3a2e1706c8e487daeff384d7837eb594d9368e8f
Brittany Morris <brittanymorrisdesign@gmail.com>
2019-11-24 20:55:47 -0500

    Added javascript files

---
+++ develop/password_generator.js
@@ -0,0 +1,25 @@
+//generate random password//
+
+function generate("length") {
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var num = "0123456789";
+    var sym = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+    var all = lowerCase + upperCase + num + sym;
+    var password = " ";
+
+    //create for loop to choose characters//
+    for(var i = 0); i < length; i = i +1);
+    var char = math.floor(math.random() * all.length);
+
+userAnswer + prompt("How many characters would you like your password to contain?");
+console.log (userAnswer);
+
+//Add password to text box//
+document.getElementById("generate").value = pasv
+
+
+}
+
+return password;
