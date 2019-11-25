95a658837da71962887f4459ff12fc5b3c6fab1d
Brittany Morris <brittanymorrisdesign@gmail.com>
2019-11-25 18:39:38 -0500

    Added arrays

--- develop/password_generator.js
+++ develop/password_generator.js
@@ -1,22 +1,38 @@
 //generate random password//
 
-function generate("length") {
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var num = "0123456789";
-    var sym = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+function generate() {
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var sym = ["#", "$", "%", "&", """, "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "_", "|", "`", "{", "}", "~"];
 
-    var all = lowerCase + upperCase + num + sym;
-    var password = " ";
+    var userAnswer = prompt("How many characters would you like your password to contain?");
 
-    //create for loop to choose characters//
-    for(var i = 0); i < length; i = i +1);
-    password = password + all.charAt(Math.floor(Math.random() * Math.floor(all.length - 1)));
+    if (userAnswer >= 8 && userAnswer <= 128) {
+    console.log("yes")
+    }
+    else {
+        console.log("no")
 
-//Add password to text box//
-document.getElementById("generate").all = password
-
-
+    }
 }
+   generate()
 
-return password;
+
+   userAnswer = prompt("How many characters would you like your password to contain?");
+   console.log(userAnswer);
+
+
+//     var all = lowerCase + upperCase + num + sym;
+//     var password = " ";
+
+//     //create for loop to choose characters//
+//     for(var i = 0); i < all.length; i = i +1);
+//     password = password + all.charAt(Math.floor(Math.random() * Math.floor(all.length - 1)));
+
+// //Add password to text box//
+// document.getElementById("generate").all = password
+
+
+
+
