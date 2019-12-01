190ec994dbe93babd69f9da976ee2f8818493db7
Brittany Morris <brittanymorrisdesign@gmail.com>
2019-12-01 15:52:37 -0500

    Updated copy to clipboard

--- develop/password_generator.js
+++ develop/password_generator.js
@@ -6,8 +6,7 @@ var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var sym = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 
-function generate()
-{
+function generate() {
     var passwordLength = document.getElementById("length").value;
     var lowerChar = document.getElementById("lowerChar").checked;
     var upperChar = document.getElementById("upperChar").checked;
@@ -15,33 +14,64 @@ function generate()
     var specialChar = document.getElementById("specialChar").checked;
     var password = "";
 
-    if(passwordLength >= 8 && passwordLength <= 128){
+    if(passwordLength >= 8 && passwordLength <= 128) {
         for(var i = 0;i<passwordLength; i++){
             password = password + ChoosingChar(lowerChar,upperChar,numChar,specialChar);
         }
 
         document.getElementById('password').innerHTML = password;
     }
-    else{
+    else {
         document.getElementById('password').innerHTML = "Password must contain 8-128 characters.";
     }
 }
 
-function ChoosingChar(lowerChar, upperChar,numChar,specialChar)
-{
-    var charChoice = "";
+function ChoosingChar(lowerChar, upperChar, numChar, specialChar) {
+
+    var charChoice = " ";
+    
     if(lowerChar === true){
-        charChoice = charChoice + "var lowerCase";
+        charChoice = charChoice.concat(lowerCase);
     }
     if(upperChar === true){
-        charChoice = charChoice + "var upperCase";
+        charChoice = charChoice.concat(upperCase);
     }
     if(numChar === true){
-        charChoice= charChoice + "var num";
+        charChoice= charChoice.concat(num);
     }
     if(specialChar === true){
-        charChoice= charChoice + "var sym";
+        charChoice = charChoice.concat(sym);
     }
     character = charChoice.charAt(Math.floor((Math.random() * charChoice.length)));
     return character;
 }
+
+ /* Copy Clipboard*/
+ function copyPassword() {
+  /* Get the text field */
+  var copyText = document.getElementById("password");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
