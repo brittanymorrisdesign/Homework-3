27c06909ac7843018fe168b92c39de47663de22a
Brittany Morris <brittanymorrisdesign@gmail.com>
2019-12-02 21:08:24 -0500

    Changed arrays to strings

--- develop/password_generator.js
+++ develop/password_generator.js
@@ -1,10 +1,8 @@
 /* Generate random password */
-
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var sym = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var num = "0123456789"
+var sym = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
 function generate() {
     //get password length
@@ -20,16 +18,16 @@ function generate() {
             password = password + ChoosingChar(lowerChar,upperChar,numChar,specialChar);
         }
 
-        document.getElementById('password').innerHTML = password;
+        document.getElementById("password").innerHTML = password;
     }
-    else { // error message appears
-        document.getElementById('password').innerHTML = "Password must contain 8-128 characters.";
+    else { // Error message appears
+        document.getElementById("password").innerHTML = "Password must contain 8-128 characters.";
     }
 }
 
 function ChoosingChar(lowerChar, upperChar, numChar, specialChar) {
 
-    var charChoice = " ";
+    var charChoice = "";
     
     if(lowerChar === true) {
         charChoice = charChoice.concat(lowerCase);
@@ -62,17 +60,3 @@ function ChoosingChar(lowerChar, upperChar, numChar, specialChar) {
   /* Alert the copied text */
   alert("Copied the text: " + copyText.value);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
